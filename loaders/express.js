5bddc88c76c94cb9d6cd849b564d3c2044c66cb9
mfmehdi <mfmehdi2@gmail.com>
2020-01-29 18:25:49 +0100

    brand signup & login +test middleware

--- loaders/express.js
+++ loaders/express.js
@@ -2,10 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const productsRouter = require("../api/products");
 const user = require("../api/user");
+const brand = require("../api/brand");
+
 
 module.exports = async app => {
   app.use(cors());
   app.use(express.json());
   app.use("/api/products", productsRouter);
-  app.use("/user", user);
+  app.use("/api/user", user);
+  app.use("/api/brand", brand);
 };
