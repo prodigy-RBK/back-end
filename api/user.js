4692139a2146a152c1be3277a66c8e9ced021b33
Maher-Bouzid <bouzid.meher@gmail.com>
2020-02-14 21:10:38 +0100

    fixed the redirect of the mail

--- api/user.js
+++ api/user.js
@@ -20,7 +20,7 @@ router.get("/confirmation/:token", confirmation, (req, res) => {
   userOperations
     .confirmation(req.user.email)
     .then(response => {
-      res.redirect("http://localhost:8080/"); //
+      res.redirect("https://prodigy-store.onrender.com"); //
     })
     .catch(err => {
       res.status(500).send(err);
